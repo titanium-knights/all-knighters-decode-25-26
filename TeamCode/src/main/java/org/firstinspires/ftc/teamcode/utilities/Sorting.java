b071bf4974d402d1cb70ff6963db492082f2224e
shuohanTK <shuohan.peng2010@gmail.com>
2025-09-06 19:37:13 -0400

    sorting util

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Sorting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Sorting.java
@@ -5,20 +5,32 @@ import static java.lang.Math.min;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Sorting {
+    public static Telemetry telemetry;
+    public static final double toPurplePos = 0.5;
+    public static final double toGreenPos = 1.0;
+    public static final double toNeutralPos = 0.0;
 
     Servo sortingServo;
-    public Sorting(HardwareMap hmap) {
+    public Sorting(HardwareMap hmap, Telemetry telemetry) {
         this.sortingServo = hmap.servo.get(CONFIG.sortingServo);
     }
 
     public void toPurple() {
-
+        sortingServo.setPosition(toPurplePos);
+        telemetry.addLine(Double.toString(sortingServo.getPosition()));
     }
 
     public void toGreen() {
+        sortingServo.setPosition(toGreenPos);
+        telemetry.addLine(Double.toString(sortingServo.getPosition()));
+    }
 
+    public void toNeutral() {
+        sortingServo.setPosition(toNeutralPos);
+        telemetry.addLine(Double.toString(sortingServo.getPosition()));
     }
 
 }

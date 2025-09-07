e411f7f5e90a1efedc620a822e47199b658c51d6
Hitanshee <hitansheesheth08@gmail.com>
2025-09-07 16:44:36 -0400

    I added the new Constants PAge for Pedro 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,18 +1,62 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
-
-import com.pedropathing.follower.Follower;
-import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.ftc.FollowerBuilder;
-import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(11.42)
+            .forwardZeroPowerAcceleration(-55.8487)
+            .lateralZeroPowerAcceleration(-72.1592)
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .centripetalScaling(0.0002)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
+            .drivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.008, 0, 0.000005, 0.6, 0)
+            )
+            .secondaryTranslationalPIDFCoefficients(
+                    new PIDFCoefficients(0.1, 0, 0.01, 0)
+            )
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
+            .secondaryDrivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.1, 0, 0, 0.6, 0)
+            );
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName(CONFIG.FRONT_LEFT)
+            .leftRearMotorName(CONFIG.BACK_LEFT)
+            .rightFrontMotorName(CONFIG.FRONT_RIGHT)
+            .rightRearMotorName(CONFIG.BACK_RIGHT)
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(53.5)
+            .yVelocity(40.7168);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-2.1875)
+            .strafePodX(-2.0625)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .yawScalar(1.0)
+            .encoderResolution(
+                    GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD
+            )
+            .customEncoderResolution(13.26291192)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            500,
+            1,
+            1
+    );
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -78,10 +78,10 @@ public class Tuning extends SelectableOpMode {
     @Override
     public void onSelect() {
         if (follower == null) {
-            follower = Constants.createFollower(hardwareMap);
+            follower = Constants_bad_kys_die.createFollower(hardwareMap);
             PanelsConfigurables.INSTANCE.refreshClass(this);
         } else {
-            follower = Constants.createFollower(hardwareMap);
+            follower = Constants_bad_kys_die.createFollower(hardwareMap);
         }
 
         follower.setStartingPose(new Pose());

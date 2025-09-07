ccfbadd1506e988153e76b817d77a7aa878845fb
shuohanTK <shuohan.peng2010@gmail.com>
2025-09-07 09:46:01 -0400

    combined teleop codes and added timed sorting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 import static java.lang.Math.abs;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 import android.widget.Button;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,6 +13,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
+import org.firstinspires.ftc.teamcode.utilities.CameraAngle;
 import org.firstinspires.ftc.teamcode.utilities.SubsystemManager;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -23,6 +27,12 @@ import org.firstinspires.ftc.teamcode.utilities.Sorting;
 @TeleOp(name="Teleop")
 public class Teleop extends OpMode {
     private SubsystemManager subsystemManager;
+    public static String color = "p";
+
+    //time stuff
+    private final ElapsedTime sortingTimer = new ElapsedTime(); //this may or may not work
+    public boolean sortingActive = false;
+
 
     @Override
     public void init() {
@@ -32,6 +42,31 @@ public class Teleop extends OpMode {
 
     @Override
     public void loop() {
+
+        if (gamepad1.left_trigger > 0.5) {
+            subsystemManager.intake.intakeRun();
+
+            if(!sortingActive){  // only reset when starting
+                sortingTimer.reset();
+                sortingActive = true;
+            }
+
+            if(color.equals("green") && sortingTimer.seconds() < 4.5){
+                subsystemManager.sorting.toGreen();
+            }
+            else if(color.equals("purple") && sortingTimer.seconds() < 4.5){
+                subsystemManager.sorting.toPurple();
+            }
+            else {
+                subsystemManager.sorting.toNeutral();
+            }
+
+        } else {
+            subsystemManager.intake.intakeStop();
+            sortingActive = false;
+        }
+
+
         //sorting is manual for now
         if (gamepad1.dpad_left) {
             subsystemManager.sorting.toGreen();
@@ -43,6 +78,32 @@ public class Teleop extends OpMode {
             subsystemManager.sorting.toPurple();
         } else {
             subsystemManager.sorting.toNeutral();
+        }
+
+        if (gamepad1.right_trigger > 0.5) {
+            subsystemManager.launch.launchRun();
+        } else {
+            subsystemManager.launch.launchStop();
+        }
+
+        if (gamepad1.right_bumper) {
+            subsystemManager.release.releaseGreen();
+        } else {
+            subsystemManager.release.releaseNeutral();
+        }
+
+        if (gamepad1.left_bumper) {
+            subsystemManager.release.releasePurple();
+        } else {
+            subsystemManager.release.releaseNeutral();
+        }
+        // this button will be temporary, instead will be replaced with releasing whatever the order
+
+        if(gamepad1.a && subsystemManager.cameraAngle.getPosition()== CameraAngle.cameraAprilTagPos){
+            subsystemManager.cameraAngle.cameraColor();
+        }
+        if(gamepad1.a && subsystemManager.cameraAngle.getPosition()==CameraAngle.cameraColorPos){
+            subsystemManager.cameraAngle.cameraAprilTag();
         }
     }
 }

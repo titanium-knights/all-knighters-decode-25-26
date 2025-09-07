7d0555c99eeaef7516f7159d1d7f3b897be6ef99
shuohanTK <shuohan.peng2010@gmail.com>
2025-09-06 21:23:43 -0400

    opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -4,6 +4,7 @@ import static java.lang.Math.abs;
 
 import android.widget.Button;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -19,16 +20,27 @@ import org.firstinspires.ftc.teamcode.utilities.Release;
 import org.firstinspires.ftc.teamcode.utilities.SimpleMecanumDrive;
 import org.firstinspires.ftc.teamcode.utilities.Sorting;
 
-public class Teleop {
+@TeleOp(name="Teleop")
+public class Teleop extends OpMode {
     private SubsystemManager subsystemManager;
 
-    public void loop(Telemetry telemetry, Gamepad gamepad1) {
+    @Override
+    public void init() {
+        subsystemManager = new SubsystemManager(hardwareMap, telemetry);
+    }
 
-        if(gamepad1.dpad_left) {
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_left) {
             subsystemManager.sorting.toGreen();
-        } else if(gamepad1.dpad_right) {
-            subsystemManager.sorting.toPurple();
+        } else {
+            subsystemManager.sorting.toNeutral();
         }
 
+        if (gamepad1.dpad_right) {
+            subsystemManager.sorting.toPurple();
+        } else {
+            subsystemManager.sorting.toNeutral();
+        }
     }
 }

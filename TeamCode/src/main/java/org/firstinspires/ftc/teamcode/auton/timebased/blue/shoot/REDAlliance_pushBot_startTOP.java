ed30c6278b4db0306a2b7ef65b99be7491a6ac1a
Hitanshee <hitansheesheth08@gmail.com>
2025-11-02 09:24:36 -0500

    Pushbot for red alliance

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/timebased/blue/shoot/REDAlliance_pushBot_startTOP.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.auton.timebased.blue.shoot;
+
+public class REDAlliance_pushBot_startTOP {
+    //TODO: This is the psuedo code for pushing balls into the outake section (STARTING from the red triangle)
+
+    //Start
+    //Move 2 square to the right (don't go forward yet, still in triangle) (in parallel with squares)
+    //Turn to face the rows (45 degrees) - because we start at an angle (face towards blue triangle)
+    //Move right (1 square)
+    //Move backward to align with the first ball (be in parallel)
+    //Move left (2 squares) (push the ball back to the triangle)
+    //Turn 90 degrees to push the balls into the wall
+    //Move left (should be pushed fully now)
+    // Move forward 2 squares
+    // Move right (2 squares)
+    // Move backward towards the balls again and align the robot
+    // Move left( all the way to the red triangle)
+    // Turn 90 degrees
+    // Move left (should be pushed fully now)
+
+
+
+}

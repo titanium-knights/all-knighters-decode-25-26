3a6d0aad6943e589bdc9e87bb6e980f7f44d3a09
Hitanshee <hitansheesheth08@gmail.com>
2025-12-08 17:12:08 -0500

    Created bottom RED take 1 auto + preload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/pedroPathing/sortingRamp/RED_sT1.java
@@ -0,0 +1,197 @@
+package org.firstinspires.ftc.teamcode.auton.pedroPathing.sortingRamp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+
+//import org.firstinspires.ftc.teamcode.utilities.Intake;
+import org.firstinspires.ftc.teamcode.auton.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.utilities.Outtake;
+
+//This code is for Red alliance from the bottom
+@Autonomous(name = "RED_sT1")
+public class RED_sT1 extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
+    public Outtake outtake;
+//    public Intake intake;
+
+    private final boolean isBlue = false;
+    private double m(double init, boolean isBlue) {
+        if (isBlue) { return init; }
+        else { return 144 - init; }
+    }
+    private double mr(double init, boolean isBlue) {
+        if (isBlue) { return Math.toRadians(init); }
+        else {
+            double result =
+                    Math.atan2(
+                            Math.sin(Math.toRadians(init)),
+                            -1 * Math.cos(Math.toRadians(init))
+                    );
+            if (result < 0) {
+                result = 360 + result;
+            }
+            return result;
+        }
+    }
+
+
+
+    private final Pose StartTOP_Red = new Pose(0, 0, Math.toRadians(90));
+
+    private final Pose midPoint_Triangle = new Pose(0, 0, Math.toRadians(90));
+    private final Pose StartBottom_RED = new Pose(96, 0,Math.toRadians(90));
+    private final Pose StartBottom_Red = new Pose(65.25, 0,Math.toRadians(90));
+
+    private final Pose humanPlayer_RED = new Pose(133.75, 0,Math.toRadians(0));
+    private final Pose humanPlayer_BLUE = new Pose(10.25, 0,Math.toRadians(180));
+
+
+    private final Pose groupCPickUp1_RED = new Pose(96, 27.375,Math.toRadians(0));
+    private final Pose groupCPickUp1_BLUE = new Pose(48,44.625,Math.toRadians(180));
+
+    private final Pose groupCPickUp2_RED = new Pose(126.75, 27.375 ,Math.toRadians(0));
+    private final Pose groupCPickUp2_BLUE = new Pose(0,44.625,Math.toRadians(180));
+
+    private final Pose groupBPickUp1_RED = new Pose(96,51.375,Math.toRadians(0));
+    private final Pose groupBPickUp1_BLUE = new Pose(48,68.625,Math.toRadians(180));
+
+    private final Pose groupBPickUp2_RED = new Pose(126.75,51.375,Math.toRadians(0));
+    private final Pose groupBPickUp2_BLUE = new Pose(0,68.625,Math.toRadians(180));
+
+    private final Pose groupAPickUp1_RED = new Pose(96,75.375,Math.toRadians(0));
+    private final Pose groupAPickUp1_BLUE = new Pose(48,92.625,Math.toRadians(180));
+
+    private final Pose groupAPickUp2_RED = new Pose(126.75,75.375,Math.toRadians(0));
+    private final Pose groupAPickUp2_BLUE = new Pose(0,92.625,Math.toRadians(180));
+
+    private final Pose shootAtBasket_RED = new Pose(120,120,Math.toRadians(45));
+    private final Pose shootAtBasket_BLUE = new Pose(24,24,Math.toRadians(135));
+
+    private final Pose initialTURN_BLUE = new Pose(130, 80, Math.toRadians(90));
+
+    private final Pose moveTowardsBasket_BLUE = new Pose(72, 130, Math.toRadians(90));
+
+    private final Pose moveTowardsBasket_RED = new Pose(25, 25,Math.toRadians(135));
+
+    private final Pose initialTURN_RED = new Pose (50, 50,Math.toRadians(120));
+    private PathChain ScorePRELOAD, ScoreFIRST, ScoreSECOND, ScoreTHIRD;
+
+    public void buildPaths(){
+
+        ScorePRELOAD = follower.pathBuilder()
+                .addPath(new BezierLine(StartBottom_RED, initialTURN_RED))
+                .setLinearHeadingInterpolation(StartBottom_RED.getHeading(), initialTURN_RED.getHeading())
+
+                .addPath(new BezierLine(initialTURN_RED, moveTowardsBasket_RED))
+                .setLinearHeadingInterpolation(initialTURN_RED.getHeading(), moveTowardsBasket_RED.getHeading())
+
+                .addPath(new BezierLine(moveTowardsBasket_RED, shootAtBasket_RED))
+                .setLinearHeadingInterpolation(moveTowardsBasket_RED.getHeading(), shootAtBasket_RED.getHeading())
+
+                //Shoot Functions
+
+                .build();
+
+        ScoreFIRST = follower.pathBuilder()
+                .addPath(new BezierLine(shootAtBasket_RED, groupAPickUp1_RED))
+                .setLinearHeadingInterpolation(shootAtBasket_RED.getHeading(), groupAPickUp1_RED.getHeading())
+
+                .addPath(new BezierLine(groupAPickUp1_RED, groupAPickUp2_RED))
+                .setLinearHeadingInterpolation(groupAPickUp1_RED.getHeading(), groupAPickUp2_RED.getHeading())
+
+                .addPath(new BezierLine(groupAPickUp2_RED, groupAPickUp1_RED))
+                .setLinearHeadingInterpolation(groupAPickUp2_RED.getHeading(), groupAPickUp1_RED.getHeading())
+
+                .addPath(new BezierLine(groupAPickUp2_RED, shootAtBasket_RED))
+                .setLinearHeadingInterpolation(groupAPickUp1_RED.getHeading(), shootAtBasket_RED.getHeading())
+
+                //Some sort of shoot functions
+
+                .build();
+
+
+
+    }
+
+    private int counter = 0;
+    public void autonomousPathUpdate() {
+        if (counter == 0) {
+            follower.followPath(ScorePRELOAD);
+            counter = 1;
+        }
+        if (counter == 1) {
+            follower.followPath(ScoreFIRST);
+            counter = 2;
+        }
+
+
+
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void loop() {
+        // These loop the movements of the robot
+        follower.update();
+        autonomousPathUpdate();
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+//        Constants.setConstants(FConstants.class, LConstants.class);
+        opmodeTimer = new Timer();
+
+        opmodeTimer.resetTimer();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(StartTOP_Red);
+
+        buildPaths();
+        // TODO: give hardware map the same name when we figure it out
+        outtake = new Outtake(hardwareMap, telemetry);
+//        intake = new Intake(hardwareMap, telemetry);
+
+        //TODO: add the position you would want everything to start at here like claw.closed()
+    }
+
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    @Override
+    public void stop() {
+    }
+}
+
+
+
+
+
+
+
+
